0307bc957c0aa19850566bdbed3a715b67474cad
unknown <you@example.com>
2021-06-07 22:58:47 +0800

    jjr

---
+++ GoldMiner/assets/Scipt/jump3.js
@@ -0,0 +1,41 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+next: function () {
+        this.node.runAction(cc.sequence(cc.fadeOut(0.5),cc.callFunc(function (){ cc.director.loadScene('scene4')})));
+    },
+   
+    start () {
+
+    },
+
+    // update (dt) {},
+});
